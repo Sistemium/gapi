d9b4f213ccb440c939cf039a2f1b0ac94c91c656
Alexander Levin <alevin.ru@gmail.com>
2020-03-22 18:32:03 +0200

    WarehouseBoxConfirmed stamps

--- src/models/WarehouseBoxConfirmed.js
+++ src/models/WarehouseBoxConfirmed.js
@@ -3,11 +3,18 @@ import ModelSchema from 'sistemium-mongo/lib/schema';
 export default new ModelSchema({
   collection: 'WarehouseBoxConfirmed',
   schema: {
+
     barcode: String,
     deviceUUID: String,
     deviceCts: String,
     warehouseBoxId: String,
     warehouseItemIds: [String],
+
+    stamps: [String],
+    dateProduction: String,
+    stockBatchBarcode: String,
+    articleId: String,
+
   },
   indexes: [{ barcode: 1 }],
   tsType: 'timestamp',
