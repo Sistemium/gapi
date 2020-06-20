3836e9cf8ed734c9b8cf04564a4d499c650d662a
Alexander Levin <alevin.ru@gmail.com>
2020-06-20 11:48:19 +0300

    action.discountMatrix

--- src/models/Action.js
+++ src/models/Action.js
@@ -1,28 +1,40 @@
 import ModelSchema from 'sistemium-mongo/lib/schema';
 import { Schema } from 'sistemium-mongo/lib/mongoose';
 
+const required = {
+  pcs: Number,
+  volume: Number,
+  volumeTo: Number,
+  sku: Number,
+  cost: Number,
+  costTo: Number,
+  isMultiple: {
+    type: Boolean,
+    default: false,
+  },
+  etc: String,
+};
+
+const axis = { id: String, ...required };
+
 const optionSchema = new Schema({
   name: String,
   commentText: String,
   ranges: [{ name: String }],
-  required: {
-    pcs: Number,
-    volume: Number,
-    volumeTo: Number,
-    sku: Number,
-    cost: Number,
-    costTo: Number,
-    isMultiple: {
-      type: Boolean,
-      default: false,
-    },
-    etc: String,
-  },
+  required,
   discountComp: Number,
   discountOwn: Number,
   discountCash: Number,
   price: Number,
   cashBonus: Number,
+  discountMatrix: {
+    type: {
+      axisX: [axis],
+      axisY: [axis],
+      values: [{ x: String, y: String, discountOwn: Number }],
+    },
+    default: null,
+  },
 });
 
 optionSchema.add({
