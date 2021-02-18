27384fac084246d35c9505a73b25fd9d35459a05
Alexander Levin <alevin.ru@gmail.com>
2021-02-18 13:54:10 +0200

    assertVar

--- src/lib/assert.js
+++ src/lib/assert.js
@@ -3,3 +3,8 @@ export default function assert(test, message = 'Assertion failed') {
     throw new Error(message);
   }
 }
+
+export function assertVar(name) {
+  assert(name, 'empty name param to assertVar function');
+  assert(process.env[name], `${name} environment variable must be set`);
+}
